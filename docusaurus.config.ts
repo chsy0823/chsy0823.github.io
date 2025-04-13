749124bef395b517ae64765002e333bc7b1118ab
chsy0823 <chsy0823@gmail.com>
2025-04-14 08:56:09 +0900

    Update github links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,11 +72,6 @@ const config: Config = {
       },
       items: [
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
@@ -87,7 +82,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/chsy0823',
             },
             {
               label: 'LinkedIn',
